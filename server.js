0d2c444accf71f68c9fb093886af1d5b9b52c7af
Raven Taylor <raven61@live..unc.edu>
2022-01-31 16:49:26 -0500

    changed port print statement

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ const server = http.createServer((req, res) => {
 // Put the exact message `Server listening on port ${port}` on the console log. 
 
 server.listen(port, () => {
-    console.log("Server listening on port $"+port)
+    console.log(`Server listening on port ${port}`)
 })
 
 
