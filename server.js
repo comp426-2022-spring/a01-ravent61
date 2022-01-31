e586cd6d9c91a6354dcb657c294e42df5a07d496
Raven Taylor <raven61@live..unc.edu>
2022-01-31 11:11:31 -0500

    line 55 test

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ fs.readFile('www/index.html', 'utf8', (err, data) => {
 const server = http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/html')
-    res.end('www/index.html')
+    res.end('<h1>Hello World</h1>')
 })
 
 
