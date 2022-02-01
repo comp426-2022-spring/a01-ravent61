cc9800db4e4bac936a7c8ee918641b414b9dff1f
Raven Taylor <raven61@live..unc.edu>
2022-02-01 15:34:30 -0500

    changed the file path

--- server.js
+++ server.js
@@ -33,11 +33,11 @@ const port = args['port'] || 3000
 
 var data1 = "" 
 
-fs.readFile('www/index.html', 'utf8', (err, data) => {
+fs.readFile('./www/index.html', 'utf8', (err, data) => {
     if (err) {
-        console.error(err)
-        return
+        console.error(err) 
         process.exit(1)
+        return
     }
     //console.log(data)
     data1 = data
